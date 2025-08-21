d51f27067c0bb66420491177a040909a94db266a
smolinivan <smolin9667@gmail.com>
2025-08-21 15:54:19 +0300

    Added gitignore

---
+++ src/pages/Landing.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Landing: React.FC = () => {
+  return (
+    <div>
+      <h1>Hello React with TypeScript and Webpack!</h1>
+      <p>Ваше приложение успешно настроено!</p>
+    </div>
+  );
+};
+
+export default Landing;
