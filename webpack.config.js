699b1fa38ff30e39e1e4d18494a600bc2dbb325e
smolinivan <smolin9667@gmail.com>
2025-09-03 14:59:30 +0300

    Added .nojekill

--- webpack.config.js
+++ webpack.config.js
@@ -43,7 +43,7 @@ module.exports = {
     },
 
     output: {
-        filename: 'bundle.[contenthash].js',
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
         publicPath: '/', // Важно для GitHub Pages
